748309925b1e674ffee9d427060c76a6f0d76822
RohitNemade11 <rohitnemade2024@gmail.com>
2025-06-17 16:24:38 +0530

    rohit

--- src/models/doctorModel.js
+++ src/models/doctorModel.js
@@ -1,22 +1,43 @@
 const db = require("../config/db");
 
 exports.getAllDoctors = (callback) => {
-    db.query("SELECT * FROM doctor", callback);
+  db.query("SELECT * FROM doctor", callback);
 };
 
 exports.deleteDoctorById = (id, callback) => {
-    db.query("DELETE FROM doctor WHERE doctor_id = ?", [id], callback);
+  db.query("DELETE FROM doctor WHERE doctor_id = ?", [id], (err, result) => {
+    if (err) {
+      console.log("Error in deleteing Doctor ", err);
+      return callback(err, null);
+    } else {
+      return callback(null, err);
+    }
+  });
 };
 
 exports.getDoctorById = (id, callback) => {
-    db.query("SELECT * FROM doctor WHERE doctor_id = ?", [id], callback);
+  db.query("SELECT * FROM doctor WHERE doctor_id = ?", [id], callback);
 };
 
 exports.updateDoctor = (data, callback) => {
-    const { doctor_name, doctor_specialization, doctor_contact, doctor_experience, status, doctor_id } = data;
-    db.query(
-        "UPDATE doctor SET doctor_name=?, doctor_specialization=?, doctor_contact=?, doctor_experience=?, status=? WHERE doctor_id=?",
-        [doctor_name, doctor_specialization, doctor_contact, doctor_experience, status, doctor_id],
-        callback
-    );
+  const {
+    doctor_name,
+    doctor_specialization,
+    doctor_contact,
+    doctor_experience,
+    status,
+    doctor_id,
+  } = data;
+  db.query(
+    "UPDATE doctor SET doctor_name=?, doctor_specialization=?, doctor_contact=?, doctor_experience=?, status=? WHERE doctor_id=?",
+    [
+      doctor_name,
+      doctor_specialization,
+      doctor_contact,
+      doctor_experience,
+      status,
+      doctor_id,
+    ],
+    callback
+  );
 };
